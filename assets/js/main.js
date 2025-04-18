e8f8927dc0eba3e19949e2cd4ec18ac90cc7bcfe
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-04-18 18:22:24 +0530

    update ai music code

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,8 @@ Version: 1.0
 	## Testimonials Slider
 	## WOW
 	## Preloader
+	## Window Scroll
+	## Cursor
 	
 ----------------------------------------------------------------------*/
 
@@ -73,7 +75,7 @@ Version: 1.0
         handleNavMenu();
       }
       
-
+  
       /******************* ## Back to Top ******************/
       if ($(".back-to-top").length) {
           $(".back-to-top").click(function () {
@@ -129,50 +131,206 @@ Version: 1.0
   });
 
   /******************* ## WOW ******************/
-  if (typeof WOW != "undefined") {
-      new WOW({}).init();
-  }
+  // if (typeof WOW != "undefined") {
+  //     new WOW({}).init();
+  // }
 
   /******************* ## Preloader ******************/
-  $(window).on("load", function () {
-      function handlePreloader() {
-          if ($(".dh-preloader").length) {
-              $(".dh-preloader").delay(0).fadeOut(700);
-          }
+  $(document).ready(function () {
+    let percent = 0;
+    const $percentEl = $("#percent");
+    const $loader = $(".loader");
+    const target = 100;
+  
+  
+    const interval = setInterval(function () {
+      if (percent < target) {
+        percent++;
+        $percentEl.text(percent + "%");
+      } else {
+        clearInterval(interval);
+  
+        $loader.fadeOut(500, function () {
+        });
       }
-      handlePreloader();
+    }, 30);
   });
 
-    /******************* ## Window Scroll ******************/
-    $(window).on("scroll", function () {
-        let windowpos = $(window).scrollTop();
-        let sticky = $(".header-area");
-    
-        function headerStyle() {
-          if ($(".header-area").length) {
-            const scrollLink = $(".back-to-top");
-            if (windowpos >= 100) {
-              scrollLink.addClass("scroll-to-target");
-            } else {
-              scrollLink.removeClass("scroll-to-target");
-            }
-          }
+  /******************* ## Window Scroll ******************/
+  $(window).on("scroll", function () {
+    let windowpos = $(window).scrollTop();
+    let sticky = $(".header-area");
+  
+    function headerStyle() {
+      if ($(".header-area").length) {
+        const scrollLink = $(".back-to-top");
+        if (windowpos >= 100) {
+          scrollLink.addClass("scroll-to-target");
+        } else {
+          scrollLink.removeClass("scroll-to-target");
         }
-        headerStyle();
-    
-        function stickyHeader() {
-          if ($(".header-area").length) {
-            if (windowpos < 100) {
-              sticky.removeClass("sticky");
-            } else {
-              sticky.addClass("sticky");
-            }
-          }
+      }
+    }
+    headerStyle();
+  
+    function stickyHeader() {
+      if ($(".header-area").length) {
+        if (windowpos < 100) {
+          sticky.removeClass("sticky");
+        } else {
+          sticky.addClass("sticky");
         }
-        stickyHeader();
+      }
+    }
+    stickyHeader();
+  });
+
+
+  /******************* ## Cursor ******************/
+  $(document).ready(function () {
+    const $cursorTag = $(".custom-cursor");
+    if ($cursorTag.length) {
+      const $balls = $cursorTag.find("div");
+      const $ballMessage = $cursorTag.find("div span");
+      const $cursorData = $("[data-cursor]");
+      const $cursorImg = $cursorTag.find(".custom-cursor__img");
+  
+      let aimX = 0;
+      let aimY = 0;
+  
+      $balls.each(function (index) {
+        let $ball = $(this);
+        let currentX = 0;
+        let currentY = 0;
+        let speed = 0.15 - index * 0.025;
+  
+        function animateCursor() {
+          currentX += (aimX - currentX) * speed;
+          currentY += (aimY - currentY) * speed;
+  
+          $ball.css({
+            left: currentX + "px",
+            top: currentY + "px",
+          });
+  
+          requestAnimationFrame(animateCursor);
+        }
+        animateCursor();
       });
+  
+      $(document).on("mousemove", function (event) {
+        aimX = event.pageX;
+        aimY = event.pageY;
+      });
+  
+      $cursorData.on("mouseover", function () {
+        let text = $(this).data("cursor");
+        $ballMessage.addClass("visible").html(text);
+        $cursorImg.addClass("visible");
+      });
+  
+      $cursorData.on("mouseout", function () {
+        $ballMessage.removeClass("visible");
+        $cursorImg.removeClass("visible");
+      });
+    }
+  });
+      
 
 
+  $(".hero-img-main").on("mousemove", function (e) {
+    if ($(window).width() > 991) {
+      const wrapper = $(this);
+      const img = wrapper.find(".hero-img");
+  
+      const offset = wrapper.offset();
+      const x = e.pageX - offset.left;
+      const y = e.pageY - offset.top;
+  
+      const centerX = wrapper.width() / 2;
+      const centerY = wrapper.height() / 2;
+  
+      const rotateX = (centerY - y) / 50;
+      const rotateY = (x - centerX) / 50;
+  
+      img.css("transform", `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+      wrapper.find(".arrow-icon").css("opacity", 1);
+    }
+  });
+  
+  $(".hero-img-main").on("mouseleave", function () {
+    const img = $(this).find(".hero-img");
+    img.css("transform", "rotateX(0deg) rotateY(0deg)");
+    $(this).find(".arrow-icon").css("opacity", 0);
+  });
 
+
+  /******************* ## Scroll Nav Menu Active ******************/
+  $(document).ready(function () {
+    const sections = $("section[id]");
+    const navLinks = $(".main-menu ul li a");
+  
+      $(window).on("scroll", function () {
+        let scrollPos = $(window).scrollTop();
+        let currentSection = null;
+    
+        sections.each(function () {
+          const sectionTop = $(this).offset().top - 100;
+          const sectionHeight = $(this).outerHeight();
+    
+          if (scrollPos >= sectionTop && scrollPos < sectionTop + sectionHeight) {
+            currentSection = $(this).attr("id");
+            return false; 
+          }
+      });
+    
+        navLinks.removeClass("active");
+    
+        if (currentSection) {
+          navLinks.each(function () {
+            const href = $(this).attr("href");
+            if (href.includes(currentSection)) {
+              $(this).addClass("active");
+            }
+          });
+        }
+      });
+  });
+  
+
+
+  $('#myTextArea').on('input keyup paste', function() {
+    var $el = $(this),
+        offset = $el.innerHeight() - $el.height();
+
+    if ($el.innerHeight() < this.scrollHeight) {
+      $el.height(this.scrollHeight - offset);
+    } else {
+      
+      $el.height(1);
+      $el.height(this.scrollHeight - offset);
+    }
+  });
+
+
+  // Add your audio/video control logic here
+  $(document).ready(function () {
+    let isPlaying = false;
+
+    $('.play-button').on('click', function () {
+      isPlaying = !isPlaying;
+
+      const $icon = $(this).find('i');
+      $icon.toggleClass('fa-play', !isPlaying);
+      $icon.toggleClass('fa-pause', isPlaying);
+
+      if (isPlaying) {
+        console.log("Playing...");
+      } else {
+        console.log("Stopped.");
+      }
+    });
+  });
     
 })(window.jQuery);
+
